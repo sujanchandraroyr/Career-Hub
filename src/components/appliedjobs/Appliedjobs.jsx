09402b191c515030b0c81803f8e6816f984a1ff7
sujanchandrar <roys33664@gmail.com>
2024-04-03 18:04:36 +0600

    filter by button applyed jobs add

--- src/components/appliedjobs/Appliedjobs.jsx
+++ src/components/appliedjobs/Appliedjobs.jsx
@@ -6,7 +6,23 @@ import { useState } from "react";
 const Appliedjobs = () => {
     const jobs = useLoaderData();
     const [appliedJobs, setAppliedJobs] = useState([])
-    useEffect(() => {
+    const [displayJobs, setDisplayJobs] = useState([])
+
+    const handleJobsFilter = filter => {
+        if(filter === 'all'){
+            setDisplayJobs(appliedJobs)
+        }
+        else if(filter === 'remote'){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote')
+            setDisplayJobs(remoteJobs)
+        }
+        else if(filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite')
+            setDisplayJobs(onsiteJobs)
+        }
+    }
+
+        useEffect(() => {
         const storedJobs = getStoredJobApplication()
         
         console.log(storedJobs)
@@ -19,27 +35,28 @@ const Appliedjobs = () => {
                 }
             }
             // const jobsApplied = jobs.filter(job => storedJobs.includes(job.id))
-            console.log(setAppliedJobs)
+            
             setAppliedJobs(jobsApplied)
+            setDisplayJobs(jobsApplied)
             
         }
-    },[])
+    },[jobs])
     return (
         <div>
             <h2>Applied Jobs: {appliedJobs.length}</h2>
 
         <details className="dropdown">
-            <summary className="m-1 btn">open or close</summary>
+            <summary className="m-1 btn">Filter By</summary>
             <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                <li><a>All job</a></li>
-                <li><a>Remote</a></li>
-                <li><a>Onsite</a></li>
+                <li onClick={() => handleJobsFilter('all')}><a>All job</a></li>
+                <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                <li onClick={() => handleJobsFilter('onsite')}><a>Onsite</a></li>
              </ul>
     </details>
 
             <div>
                 {
-                   appliedJobs.map(job => <li key={job.id}>
+                   displayJobs.map(job => <li key={job.id}>
                     <span>{job.job_title}</span>
                     <span className="text-xl font-medium">{job.company_name}</span>:
                     <span>{job.remote_or_onsite}</span>
